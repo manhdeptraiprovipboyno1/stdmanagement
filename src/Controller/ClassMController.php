e9b80c98dbd6625ebaa33e694c7e3862a1a78dec
ducanhht00 <ducanhht847@gmail.com>
2021-10-25 11:22:41 +0700

    Update ClassMController.php

--- src/Controller/ClassMController.php
+++ src/Controller/ClassMController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ClassMController extends AbstractController
 {
-    #[Route('/classm', name: 'class_m')]
+    #[Route('/class/m', name: 'class_m')]
     public function index(): Response
     {
         return $this->render('class_m/index.html.twig', [
